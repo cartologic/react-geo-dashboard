6e93bc59cb553f85b03b546889f072ee38ee77bc
Unknown <ibrahim.a.hamid@gmail.com>
2019-01-19 01:00:29 +0200

    Redux configured and ready

--- src/index.js
+++ src/index.js
@@ -9,7 +9,26 @@ import "../node_modules/react-resizable/css/styles.css";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import thunk from 'redux-thunk';
+
+import dashboardReducer from './store/reducers/Dashboard';
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || null : compose;
+const rootReducer = combineReducers({
+    dashboardReducer: dashboardReducer,
+});
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
+const app = (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
