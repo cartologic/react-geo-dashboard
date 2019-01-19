322bbd0bfe2ec0d1b4c1d801c825b1d66313b408
Unknown <ibrahim.a.hamid@gmail.com>
2019-01-19 15:23:08 +0200

    Configure Dashboard Modal Functional

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,35 +1,40 @@
 import React, {Component} from 'react';
 import DefaultDashboardLogo from "../../assets/img/others/default-dashboard.png";
-import {
-    Col,
-    CardBody,
-    CardHeader,
-    Card,
-    CardFooter,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    Button
+import {Col, CardBody, CardHeader, Card, CardFooter, Modal, ModalHeader,
+    ModalBody, ModalFooter, Button, Form, FormGroup, Label, Input,
 } from "reactstrap";
 import Moment from 'react-moment';
-
 
 class Dashboard extends Component {
     constructor(props) {
         super(props);
         this.toggleDeleteModal = this.toggleDeleteModal.bind(this);
+        this.toggleConfigureModal = this.toggleConfigureModal.bind(this);
         this.state = {
             title: this.props.dashboardObject.title,
+            newTitle: this.props.dashboardObject.title,
             id: this.props.dashboardObject.id,
             date_created: this.props.dashboardObject.date_created,
             deleteModalOpen: false,
+            configureModalOpen: false,
         };
     }
 
     toggleDeleteModal() {
         this.setState({
             deleteModalOpen: !this.state.deleteModalOpen
+        });
+    }
+
+    toggleConfigureModal() {
+        this.setState({
+            configureModalOpen: !this.state.configureModalOpen
+        });
+    }
+
+    setDashboardTitle = (event) => {
+        this.setState({
+            newTitle: event.target.value
         });
     }
 
@@ -47,11 +52,29 @@ class Dashboard extends Component {
                         <Button color="secondary" onClick={this.toggleDeleteModal}>Cancel</Button>
                     </ModalFooter>
                 </Modal>
+                <Modal isOpen={this.state.configureModalOpen}
+                       className={'modal-primary'}>
+                    <ModalHeader toggle={this.toggleConfigureModal}>Configure Dashboard</ModalHeader>
+                    <ModalBody>
+                        <Form action="" method="post" className="form-horizontal">
+                            <FormGroup row>
+                                <Col md="2"><Label htmlFor="dashboard-title">Title</Label></Col>
+                                <Col xs="12" md="10">
+                                    <Input value={this.state.newTitle} onChange={this.setDashboardTitle} type="text" id="dashboard-title" name="dashboard-title" placeholder="Please enter title for dashboard" />
+                                </Col>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary">Save</Button>{' '}
+                        <Button color="secondary" onClick={this.toggleConfigureModal}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
                 <Card>
                     <CardHeader>
                         <a href={"#/dashboards/"+this.state.id}>{this.state.title}</a>
                         <div className="card-header-actions">
-                            <button className="card-header-action btn btn-setting"><i className="icon-settings"></i></button>
+                            <button onClick={this.toggleConfigureModal} className="card-header-action btn btn-setting"><i className="icon-settings"></i></button>
                             <button onClick={this.toggleDeleteModal} className="card-header-action btn btn-close"><i className="icon-close"></i></button>
                         </div>
                     </CardHeader>
