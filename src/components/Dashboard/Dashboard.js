ecc06dacf8ac2cc8dbf9b02fe646932f57bbf301
Unknown <ibrahim.a.hamid@gmail.com>
2019-01-20 13:24:19 +0200

    Dashboard now has a theme option

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -15,6 +15,8 @@ class Dashboard extends Component {
         this.state = {
             title: this.props.dashboardObject.title,
             newTitle: this.props.dashboardObject.title,
+            theme: this.props.dashboardObject.theme,
+            newTheme: this.props.dashboardObject.theme,
             id: this.props.dashboardObject.id,
             date_created: this.props.dashboardObject.date_created,
             deleteModalOpen: false,
@@ -40,11 +42,18 @@ class Dashboard extends Component {
         });
     }
 
+    setDashboardTheme = (event) => {
+        this.setState({
+            newTheme: event.target.value
+        });
+    }
+
     saveDashboard = () => {
         const dashboardObject = {
             date_created: this.state.date_created,
             id: this.state.id,
             title: this.state.newTitle,
+            theme: this.state.newTheme,
         }
         this.props.onSaveDashboard(dashboardObject);
     }
@@ -52,7 +61,7 @@ class Dashboard extends Component {
     render() {
         return (
             <Col xs="12" sm="3" md="4" key={this.state.id} id={this.state.id}>
-                <Modal isOpen={this.state.deleteModalOpen} toggle={this.toggleDeleteModal}
+                <Modal isOpen={this.state.deleteModalOpen} toggle={this.toggleDeleteModal} // Delete Modal
                        className={'modal-danger'}>
                     <ModalHeader toggle={this.toggleDeleteModal}>Delete Dashboard {this.state.title} ?</ModalHeader>
                     <ModalBody>
@@ -63,7 +72,7 @@ class Dashboard extends Component {
                         <Button color="secondary" onClick={this.toggleDeleteModal}>Cancel</Button>
                     </ModalFooter>
                 </Modal>
-                <Modal isOpen={this.state.configureModalOpen}
+                <Modal isOpen={this.state.configureModalOpen} // Configure Modal
                        className={'modal-primary'}>
                     <ModalHeader toggle={this.toggleConfigureModal}>Configure Dashboard</ModalHeader>
                     <ModalBody>
@@ -74,6 +83,22 @@ class Dashboard extends Component {
                                     <Input value={this.state.newTitle} onChange={this.setDashboardTitle} type="text" id="dashboard-title" name="dashboard-title" placeholder="Please enter title for dashboard" />
                                 </Col>
                             </FormGroup>
+                            <FormGroup row>
+                                <Col md="2">
+                                    <Label htmlFor="themeSelect">Theme</Label>
+                                </Col>
+                                <Col xs="12" md="10">
+                                    <Input type="select" name="themeSelect" id="themeSelect" onChange={this.setDashboardTheme} value={this.state.newTheme} className={this.state.newTheme}>
+                                        <option value="">Default</option>
+                                        <option className="bg-primary" value="border-primary">Border Primary</option>
+                                        <option className="bg-secondary" value="border-secondary">Border Secondary</option>
+                                        <option className="bg-success" value="border-success">Border Success</option>
+                                        <option className="bg-info" value="border-info">Border Info</option>
+                                        <option className="bg-warning" value="border-warning">Border Warning</option>
+                                        <option className="bg-danger" value="border-danger">Border Danger</option>
+                                    </Input>
+                                </Col>
+                            </FormGroup>
                         </Form>
                     </ModalBody>
                     <ModalFooter>
@@ -81,8 +106,8 @@ class Dashboard extends Component {
                         <Button color="secondary" onClick={this.toggleConfigureModal}>Cancel</Button>
                     </ModalFooter>
                 </Modal>
-                <Card>
-                    <CardHeader>
+                <Card className={this.state.theme}>
+                    <CardHeader className={this.state.theme}>
                         <a href={"#/dashboards/"+this.state.id}>{this.state.title}</a>
                         <div className="card-header-actions">
                             <button onClick={this.toggleConfigureModal} className="card-header-action btn btn-setting"><i className="icon-settings"></i></button>
@@ -91,7 +116,7 @@ class Dashboard extends Component {
                     </CardHeader>
                     <CardBody><img className="card-img-bottom" src={DefaultDashboardLogo}
                                    alt="Default Dashboard"/></CardBody>
-                    <CardFooter>
+                    <CardFooter className={this.state.theme}>
                         Created - <Moment interval={0} format="DD MMM YYYY - hh:mm a" date={this.state.date_created}/>
                     </CardFooter>
                 </Card>
